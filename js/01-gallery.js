959f1140e19e33df4d823ae4529015a530139e9e
Velorian-TDV <terleckydima92@gmail.com>
2023-05-21 20:14:07 +0300

    add close evenet for popup when escape are pressed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,32 +1,22 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-// console.log
-const $ = (item, type) => {
-
-    if (type === true) {
-
-        return console.log(typeof item)
-
-    } else {
-
-        return console.log(item)
-
-    }
-
-}
-
-// variabels
 const gallery = document.querySelector('.gallery');
 
 // create element function
 function createElement(preview, original, alt,) {
     return (`
-    <li class='gallery__item'>
-        <a class='gallery__link'>
-            <img src="${preview}" alt="${alt}" class='gallery__image' data-original='${original}'>        
-        </a>
-    </li>`);
+    <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+        <img
+        class="gallery__image"
+        src="${preview}"
+        data-source="${original}"
+        alt="${alt}"
+        />
+    </a>
+    </li>
+    `);
 }
 
 // create gallary function
@@ -47,10 +37,26 @@ gallery.insertAdjacentHTML('beforeend', createGallery.join(''));
 document.querySelector('.gallery').onclick = event => {
 
     if (event.target.classList.value === 'gallery__image') {
-        
-        basicLightbox.create(`
-		    <img width="1280" height="853" src="${event.target.dataset.original}">
-	    `).show()
+
+        event.preventDefault()
+
+        const instance = basicLightbox.create(`
+		    <img width="1280" height="853" src="${event.target.dataset.source}">
+	    `, {
+            onShow: (instance) => {
+
+                document.onkeydown = event => {
+
+                    if (event.key === 'Escape') {
+                        instance.close();
+                    }
+
+                }
+
+            }
+        })
+
+        instance.show();
 
     }
 
