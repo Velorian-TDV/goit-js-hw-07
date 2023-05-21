ee1e090035cc4713e38ff87e2913ea84b948fda7
Velorian-TDV <terleckydima92@gmail.com>
2023-05-21 20:59:36 +0300

    complete second task

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,39 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector('.gallery');
+const arrayForImages = [];
+
+// create element function
+function createElement(preview, original, alt,) {
+    return (`
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+            <img class="gallery__image" src="${preview}" alt="${alt}" />
+        </a>
+    </li>
+    `);
+}
+
+// create gallary function
+const createGallery = galleryItems.map(elem => {
+
+    arrayForImages.push(elem.original);
+
+    return (
+        createElement(elem.preview, elem.original, elem.description)
+    )
+
+})
+
+// create preview gallery
+gallery.insertAdjacentHTML('beforeend', createGallery.join(''));
+
+let carosel = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250
+});
+
+carosel.on('error.simplelightbox', function (e) {
+    console.log(e); 
+});
