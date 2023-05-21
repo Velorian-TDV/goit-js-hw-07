ac7db1ec1ba7115f3077ada509b6e8997c0f0b63
Velorian-TDV <terleckydima92@gmail.com>
2023-05-21 19:07:35 +0300

    complete first task

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,57 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log
+const $ = (item, type) => {
+
+    if (type === true) {
+
+        return console.log(typeof item)
+
+    } else {
+
+        return console.log(item)
+
+    }
+
+}
+
+// variabels
+const gallery = document.querySelector('.gallery');
+
+// create element function
+function createElement(preview, original, alt,) {
+    return (`
+    <li class='gallery__item'>
+        <a class='gallery__link'>
+            <img src="${preview}" alt="${alt}" class='gallery__image' data-original='${original}'>        
+        </a>
+    </li>`);
+}
+
+// create gallary function
+const createGallery = galleryItems.map(elem => {
+
+    return (
+        createElement(elem.preview, elem.original, elem.description)
+    )
+
+})
+
+
+// create preview gallery
+gallery.insertAdjacentHTML('beforeend', createGallery.join(''));
+
+
+// popup 
+document.querySelector('.gallery').onclick = event => {
+
+    if (event.target.classList.value === 'gallery__image') {
+        
+        basicLightbox.create(`
+		    <img width="1280" height="853" src="${event.target.dataset.original}">
+	    `).show()
+
+    }
+
+}
